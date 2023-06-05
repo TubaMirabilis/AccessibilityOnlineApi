e0383d681c204f125f1d60b53ae7ee06d0583af6
TubaMirabilis <bkingsleygreen@gmail.com>
2023-06-05 05:13:51 +0100

    Starting my journey through the labyrinth of Mediator

--- src/AccessibilityOnlineApi.Api/Program.cs
+++ src/AccessibilityOnlineApi.Api/Program.cs
@@ -1,12 +1,12 @@
 using AccessibilityOnlineApi.Application;
-using AccessibilityOnlineApi.Application.Dtos;
-using AccessibilityOnlineApi.Application.Users;
 using AccessibilityOnlineApi.Infrastructure;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables(prefix: "AOAPI_");
+builder.Services.AddMediator(options =>
+{
+    options.ServiceLifetime = ServiceLifetime.Scoped;
+});
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddCors(options =>
@@ -20,12 +20,8 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
-app.MapPost("/users", async (CreateUserDTO dto, UserRegistrationHandler registrationHandler, HttpRequest request) =>
-{
-    return await registrationHandler.HandleUserRegistration(dto, request);
-})
-.Produces(StatusCodes.Status201Created)
-.Produces(StatusCodes.Status400BadRequest)
-.Produces(StatusCodes.Status401Unauthorized)
-.Produces(StatusCodes.Status409Conflict);
+// app.MapPost("/users", async (CreateUserDTO dto, UserRegistrationHandler registrationHandler, HttpRequest request) =>
+// {
+//     return await registrationHandler.HandleUserRegistration(dto, request);
+// });
 app.Run();
